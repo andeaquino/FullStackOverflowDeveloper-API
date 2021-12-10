1c4d792c86ecdb87634cc5e814f660a8d2ae51d4
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-12-10 13:02:26 -0300

    created jwt authorization

---
+++ src/middleware/auth.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+interface UserInfoRequest extends Request {
+    user: number;
+}
+
+async function auth(req: UserInfoRequest, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  const secretKey = process.env.JWT_SECRET;
+
+  if (!token) return res.status(401).send("No token provided");
+
+  jwt.verify(token, secretKey, function (err, decoded) {
+    if (err) return res.status(500).send("Failed to authenticate token");
+
+    req.user = decoded.user;
+    next();
+  });
+}
+
+export default auth;
